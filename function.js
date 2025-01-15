cc47d57f905761c938984199f0a1d462bd1e5331
Atul Kumar Sharma <49794348+AtulKrSharma@users.noreply.github.com>
2025-01-14 22:18:58 -0500

    Update function.js

--- function.js
+++ function.js
@@ -1,9 +1,10 @@
-const greeting = ({ name, addresss }) =>
-  `my name is ${name} and stay in ${addresss}`;
+const greeting = ({ name, addresss, year }) =>
+  `my name is ${name} and stay in ${addresss} & ${year}`;
 
 console.log(
   greeting({
     name: 'atul',
     addresss: 'Canada',
+    year: 2025
   })
 );
